8a3515f64fe50abbd75b76a27b776f44bb455ef3
Harley Hanna <harleyhannawrit@gmail.com>
2020-10-24 19:50:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,11 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
-    
+    @num2 = 0
+    until @num2 == @num
+      "#{@phrase}"
+      @num2 += 1
+    end
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
